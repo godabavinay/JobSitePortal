1d24220d02938c1a2547308ae4282cb0c594a6f4
Vinay Godaba <vinay@pop-os.localdomain>
2021-02-13 09:06:57 +0530

    getting ready for production

---
+++ backend/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://pandaTester:pandaTester@jobsitedb.vq6yb.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    JWT_SECRET_KEY: 'fdhafjksaf'
+}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -10,6 +11,13 @@ app.use(express.json())
 
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
+
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve()
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
 
 app.listen(PORT, () => {
     console.log(`Listening on Port number ${PORT}`)
